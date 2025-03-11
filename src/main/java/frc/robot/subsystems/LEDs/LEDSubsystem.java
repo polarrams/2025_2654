90fd833bc0b8701495a35657e78460e18cdbf506
Swright1308 <wrighste000@roseauschool.org>
2025-03-10 19:06:30 -0500

    test l3 color fix

--- src/main/java/frc/robot/subsystems/LEDs/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDs/LEDSubsystem.java
@@ -168,7 +168,7 @@ public class LEDSubsystem extends SubsystemBase {
                 case "Lime":
                 for (var i = 0; i < m_ledBuffer.getLength(); i++) {
                     //Sets LED rgb values for red
-                    m_ledBuffer.setRGB(i, 48, 201, 75);
+                    m_ledBuffer.setRGB(i,174 , 201, 48);
                     SmartDashboard.putNumber("reefdebug", 6);
                     }
                 default:
